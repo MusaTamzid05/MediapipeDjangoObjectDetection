1bf893b671e5d1371eeb494bd3fca86c20757770
MusaTamzid05 <tamzid101@gmail.com>
2024-01-22 02:08:05 +0600

    Add Car model

--- gallary/models.py
+++ gallary/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Photo(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to="photo")
+    processed_image = models.ImageField(default="black.jpg")
+
