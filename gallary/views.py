ba919b9a539e998fd28d7ae7db2762169c7f5754
MusaTamzid05 <tamzid101@gmail.com>
2024-01-22 02:18:31 +0600

    Add detail page

--- gallary/views.py
+++ gallary/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from .models import Photo
 
@@ -6,4 +7,8 @@ from .models import Photo
 def home(request):
     photos = Photo.objects.all()
     return render(request, "home.html" , {"photos" : photos})
-    
+
+
+def detail(request, photo_id):
+    photo = get_object_or_404(Photo,pk=photo_id)
+    return render(request, "detail.html", {"photo" : photo})
