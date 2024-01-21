d737f99b0630927140dfb3d86323b3baeef61b47
MusaTamzid05 <tamzid101@gmail.com>
2024-01-22 02:10:39 +0600

    Add Car model

--- gallary/views.py
+++ gallary/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Photo
 
 
 def home(request):
-    return render(request, "home.html")
+    photos = Photo.objects.all()
+    return render(request, "home.html" , {"photos" : photos})
     
